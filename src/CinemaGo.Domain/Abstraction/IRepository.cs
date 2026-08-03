8dc9bff4e6046a4a8ff40cf682e88db0d3a1b3e7
thanhdat-vu <20521181@gm.uit.edu.vn>
2026-08-03 18:09:30 +0700

    Add generic repository interface

---
+++ src/CinemaGo.Domain/Abstraction/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace CinemaGo.Domain
+{
+    internal interface IRepository<TEntity> where TEntity : class
+    {
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+        Task<TEntity?> GetByIdAsync(Guid id, CancellationToken ct = default);
+        Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken ct = default);
+        Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken ct = default);
+        IQueryable<TEntity> GetQueryFilter();
+    }
+}
