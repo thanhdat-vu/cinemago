876179d271f020733dca7593125ee20d9f775be0
thanhdat-vu <20521181@gm.uit.edu.vn>
2026-08-04 09:47:24 +0700

    Update BaseEntity: add domain event list

--- src/CinemaGo.Domain/Abstraction/BaseEntity.cs
+++ src/CinemaGo.Domain/Abstraction/BaseEntity.cs
@@ -5,5 +5,13 @@
         public Guid Id { get; set; }
         public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.Now;
         public uint Version { get; set; }
+
+        private readonly List<IDomainEvent> _events = [];
+        public IReadOnlyCollection<IDomainEvent> Events => _events.AsReadOnly();
+
+        public void RaiseEvent(IDomainEvent @event)
+        {
+            _events.Add(@event);
+        }
     }
 }
