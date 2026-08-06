49fe945b040836f0d0ae71bb2b4fce6e3732689a
thanhdat-vu <20521181@gm.uit.edu.vn>
2026-08-06 15:45:25 +0700

    Add domain unit tests for Booking and Customer

---
+++ tests/CinemaGo.UnitTests/EntityTests/BookingTests.cs
@@ -0,0 +1,224 @@
+﻿using CinemaGo.Domain;
+using CinemaGo.UnitTests.Shared;
+using FluentAssertions;
+
+namespace CinemaGo.UnitTests.EntityTests
+{
+    public class BookingTests
+    {
+        [Fact]
+        public void AddTicket_Should_IncreaseOriginAmount_When_LockMatchesSessionId()
+        {
+            var showTimeId = Guid.CreateVersion7();
+            var customer = DomainTestBuilders.GuestCustomer("sess-1");
+            var booking = DomainTestBuilders.PendingBooking(showTimeId, customer);
+            var ticket = DomainTestBuilders.LockingTicket(showTimeId, 50_000m, "sess-1");
+
+            booking.AddTicket(ticket);
+
+            booking.Tickets.Should().HaveCount(1);
+            booking.OriginAmount.Should().Be(50_000m);
+        }
+
+        [Fact]
+        public void AddTicket_Should_IncreaseOriginAmount_When_LockMatchesCustomerIdString()
+        {
+            var showTimeId = Guid.CreateVersion7();
+            var customer = DomainTestBuilders.GuestCustomer();
+            var booking = DomainTestBuilders.PendingBooking(showTimeId, customer);
+            var ticket = DomainTestBuilders.LockingTicket(showTimeId, 40_000m, customer.Id.ToString());
+
+            booking.AddTicket(ticket);
+
+            booking.OriginAmount.Should().Be(40_000m);
+        }
+
+        [Fact]
+        public void AddTicket_Should_Throw_When_CustomerIsNull()
+        {
+            var booking = new Booking
+            {
+                Id = Guid.CreateVersion7(),
+                ShowTimeId = Guid.CreateVersion7(),
+                CustomerName = "X",
+                Status = BookingStatus.Pending,
+                Customer = null
+            };
+            var ticket = DomainTestBuilders.LockingTicket(booking.ShowTimeId, 1m, "a");
+
+            var act = () => booking.AddTicket(ticket);
+            act.Should().Throw<InvalidOperationException>().WithMessage("*Customer information is required*");
+        }
+
+        [Fact]
+        public void AddTicket_Should_Throw_When_TicketShowTimeDoesNotMatchBooking()
+        {
+            var customer = DomainTestBuilders.GuestCustomer();
+            var booking = DomainTestBuilders.PendingBooking(Guid.CreateVersion7(), customer);
+            var ticket = DomainTestBuilders.LockingTicket(Guid.CreateVersion7(), 1m, customer.SessionId);
+
+            var act = () => booking.AddTicket(ticket);
+            act.Should().Throw<InvalidOperationException>().WithMessage("*same showtime*");
+        }
+
+        [Fact]
+        public void AddConcession_Should_IncreaseOriginAmount_When_ConcessionIsAvailable()
+        {
+            var booking = DomainTestBuilders.PendingBooking(Guid.CreateVersion7(), DomainTestBuilders.GuestCustomer());
+            var concession = new Concession
+            {
+                Id = Guid.CreateVersion7(),
+                Name = "Popcorn",
+                Price = 30_000m,
+                IsAvailable = true
+            };
+
+            booking.AddConcession(concession, 2);
+
+            booking.OriginAmount.Should().Be(60_000m);
+            booking.Concessions.Should().HaveCount(1);
+        }
+
+        [Fact]
+        public void AddConcession_Should_Throw_When_ConcessionIsNotAvailable()
+        {
+            var booking = DomainTestBuilders.PendingBooking(Guid.CreateVersion7(), DomainTestBuilders.GuestCustomer());
+            var concession = new Concession
+            {
+                Id = Guid.CreateVersion7(),
+                Name = "Soda",
+                Price = 10_000m,
+                IsAvailable = false
+            };
+
+            var act = () => booking.AddConcession(concession, 1);
+            act.Should().Throw<InvalidOperationException>();
+        }
+
+        [Fact]
+        public void AddConcession_Should_Throw_When_QuantityIsNotPositive()
+        {
+            var booking = DomainTestBuilders.PendingBooking(Guid.CreateVersion7(), DomainTestBuilders.GuestCustomer());
+            var concession = new Concession
+            {
+                Id = Guid.CreateVersion7(),
+                Name = "Soda",
+                Price = 10_000m,
+                IsAvailable = true
+            };
+
+            var act = () => booking.AddConcession(concession, 0);
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void Confirm_Should_MarkTicketsSoldAndRaiseEvent_When_StatusIsPending()
+        {
+            var showTimeId = Guid.CreateVersion7();
+            var customer = DomainTestBuilders.GuestCustomer();
+            var booking = DomainTestBuilders.PendingBooking(showTimeId, customer);
+            var ticket = DomainTestBuilders.LockingTicket(showTimeId, 55_000m, customer.SessionId);
+            booking.AddTicket(ticket);
+            booking.UpdateFinalAmount(5_000m);
+
+            booking.Confirm();
+
+            booking.Status.Should().Be(BookingStatus.Confirmed);
+            ticket.Status.Should().Be(TicketStatus.Sold);
+            ticket.BookingId.Should().Be(booking.Id);
+            booking.Events.Should().ContainSingle().Which.Should().BeOfType<BookingConfirmed>();
+        }
+
+        [Fact]
+        public void Confirm_Should_Throw_When_StatusIsNotPending()
+        {
+            var booking = DomainTestBuilders.PendingBooking(Guid.CreateVersion7(), DomainTestBuilders.GuestCustomer());
+            booking.Status = BookingStatus.Confirmed;
+
+            var act = () => booking.Confirm();
+            act.Should().Throw<InvalidOperationException>();
+        }
+
+        [Fact]
+        public void Cancel_Should_ReleaseTicketsAndRaiseEvent_When_StatusIsPending()
+        {
+            var showTimeId = Guid.CreateVersion7();
+            var customer = DomainTestBuilders.GuestCustomer();
+            var booking = DomainTestBuilders.PendingBooking(showTimeId, customer);
+            var ticket = DomainTestBuilders.LockingTicket(showTimeId, 50_000m, customer.SessionId);
+            booking.AddTicket(ticket);
+
+            booking.Cancel();
+
+            booking.Status.Should().Be(BookingStatus.Cancelled);
+            ticket.Status.Should().Be(TicketStatus.Available);
+            booking.Events.Should().ContainSingle().Which.Should().BeOfType<BookingCancelled>();
+        }
+
+        [Fact]
+        public void Cancel_Should_Throw_When_StatusIsAlreadyCancelled()
+        {
+            var booking = DomainTestBuilders.PendingBooking(Guid.CreateVersion7(), DomainTestBuilders.GuestCustomer());
+            booking.Status = BookingStatus.Cancelled;
+
+            var act = () => booking.Cancel();
+            act.Should().Throw<InvalidOperationException>();
+        }
+
+        [Fact]
+        public void Cancel_Should_Throw_When_StatusIsCheckedIn()
+        {
+            var booking = DomainTestBuilders.PendingBooking(Guid.CreateVersion7(), DomainTestBuilders.GuestCustomer());
+            booking.Status = BookingStatus.CheckedIn;
+
+            var act = () => booking.Cancel();
+            act.Should().Throw<InvalidOperationException>();
+        }
+
+        [Fact]
+        public void CheckIn_Should_Throw_When_StatusIsNotConfirmed()
+        {
+            var booking = DomainTestBuilders.PendingBooking(Guid.CreateVersion7(), DomainTestBuilders.GuestCustomer());
+
+            var act = () => booking.CheckIn();
+            act.Should().Throw<InvalidOperationException>();
+        }
+
+        [Fact]
+        public void CheckIn_Should_SetCheckedInWithEvent_When_StatusIsConfirmed()
+        {
+            var booking = DomainTestBuilders.PendingBooking(Guid.CreateVersion7(), DomainTestBuilders.GuestCustomer());
+            booking.Status = BookingStatus.Confirmed;
+
+            booking.CheckIn();
+
+            booking.Status.Should().Be(BookingStatus.CheckedIn);
+            booking.Events.Should().Contain(e => e is BookingCheckedIn);
+        }
+
+        [Theory]
+        [InlineData(100, 30, 70)]
+        [InlineData(100, 100, 0)]
+        [InlineData(100, 150, 0)]
+        public void UpdateFinalAmount_Should_ClampToZero_When_DiscountMeetsOrExceedsOrigin(
+            decimal origin,
+            decimal discount,
+            decimal expectedFinal)
+        {
+            var booking = DomainTestBuilders.PendingBooking(Guid.CreateVersion7(), DomainTestBuilders.GuestCustomer());
+            booking.OriginAmount = origin;
+
+            booking.UpdateFinalAmount(discount);
+
+            booking.FinalAmount.Should().Be(expectedFinal);
+        }
+
+        [Fact]
+        public void UpdateFinalAmount_Should_Throw_When_DiscountIsNegative()
+        {
+            var booking = DomainTestBuilders.PendingBooking(Guid.CreateVersion7(), DomainTestBuilders.GuestCustomer());
+            var act = () => booking.UpdateFinalAmount(-1);
+            act.Should().Throw<ArgumentException>();
+        }
+    }
+}
---
+++ tests/CinemaGo.UnitTests/EntityTests/CustomerTests.cs
@@ -0,0 +1,30 @@
+﻿using CinemaGo.Domain;
+using CinemaGo.UnitTests.Shared;
+using FluentAssertions;
+
+namespace CinemaGo.UnitTests.EntityTests
+{
+    public class CustomerTests
+    {
+        [Fact]
+        public void Register_Should_SetRegisteredAndRaiseEvent_When_CustomerIsGuest()
+        {
+            var customer = DomainTestBuilders.GuestCustomer();
+
+            customer.Register();
+
+            customer.IsRegistered.Should().BeTrue();
+            customer.Events.Should().ContainSingle().Which.Should().BeOfType<CustomerRegistered>();
+        }
+
+        [Fact]
+        public void Register_Should_Throw_When_CustomerIsAlreadyRegistered()
+        {
+            var customer = DomainTestBuilders.GuestCustomer();
+            customer.Register();
+
+            var act = () => customer.Register();
+            act.Should().Throw<InvalidOperationException>();
+        }
+    }
+}
