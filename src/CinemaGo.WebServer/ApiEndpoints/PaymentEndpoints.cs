eb8c11427899db34e198f528d136436594ed4996
thanhdat-vu <20521181@gm.uit.edu.vn>
2026-08-20 15:30:02 +0700

    chore: move sample requests to ApiEndpoints folders

---
+++ src/CinemaGo.WebServer/ApiEndpoints/PaymentEndpoints.cs
@@ -0,0 +1,19 @@
+﻿namespace CinemaGo.WebServer.ApiEndpoints
+{
+    public static class PaymentEndpoints
+    {
+        public static void MapPaymentEndpoints(this WebApplication app)
+        {
+            var group = app.MapGroup("/api/payments")
+                .WithTags("Payments");
+
+            group.MapGet("/momo-callback", MomoCallback)
+                .AllowAnonymous();
+        }
+
+        public static async Task<IResult> MomoCallback()
+        {
+            return Results.Ok();
+        }
+    }
+}
