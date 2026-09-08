4f8147bf4bda9adee475641184b781f354cfbdce
thanhdat-vu <20521181@gm.uit.edu.vn>
2026-09-08 20:21:07 +0700

    fix: resolve timezone issue

--- src/CinemaGo.Domain/Entities/ShowTime.cs
+++ src/CinemaGo.Domain/Entities/ShowTime.cs
@@ -70,7 +70,7 @@
                 Movie = movie,
                 ScreenId = screen.Id,
                 Screen = screen,
-                Date = DateOnly.FromDateTime(startAt.DateTime),
+                Date = DateOnly.FromDateTime(startAt.ToOffset(TimeSpan.FromHours(7)).DateTime),
                 StartAt = startAt,
                 EndAt = endAt,
                 Status = ShowTimeStatus.Upcoming
--- src/CinemaGo.WebServer/Controllers/ShowTimeController.cs
+++ src/CinemaGo.WebServer/Controllers/ShowTimeController.cs
@@ -41,22 +41,20 @@ namespace CinemaGo.WebServer.Controllers
         /// Processes a request to schedule a new Showtime via AJAX.
         /// </summary>
         [HttpPost]
-        public async Task<IActionResult> Create(Guid movieId, Guid screenId, DateTime startAt)
+        public async Task<IActionResult> Create(Guid movieId, Guid screenId, DateTimeOffset startAt)
         {
-            var startAtUtc = DateTime.SpecifyKind(startAt, DateTimeKind.Utc);
-
             var command = new AddShowTimeCommand
             {
                 MovieId = movieId,
                 ScreenId = screenId,
-                StartAt = new DateTimeOffset(startAtUtc)
+                StartAt = startAt.ToUniversalTime()
             };
 
             try
             {
                 var showtimeId = await bus.InvokeAsync<Guid>(command);
-                var dateString = DateOnly.FromDateTime(startAtUtc).ToString("yyyy-MM-dd");
-                return Json(new { success = true, message = "Showtime scheduled successfully!", id = showtimeId, date = dateString });
+                var dateString = startAt.ToUniversalTime().ToString("yyyy-MM-dd");
+                return Json(new { success = true, message = "Showtime scheduled successfully!", id = showtimeId });
             }
             catch (Exception ex)
             {
