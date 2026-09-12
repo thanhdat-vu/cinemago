069072612c3be01ca6e1d70b6d2921bfc25e8868
thanhdat-vu <20521181@gm.uit.edu.vn>
2026-09-12 16:37:47 +0700

    feat(showtime): add auto schedule change showtime status

---
+++ src/CinemaGo.Application/Messaging/ShowtimeEventHandlers/ShowtimeCreatedHandlers.cs
@@ -0,0 +1,23 @@
+﻿using CinemaGo.Application.Features;
+
+namespace CinemaGo.Application.Messaging
+{
+    public class StatusShowtimeCreatedHandler(IMessageBus bus, IUnitOfWork uow)
+    {
+        public async Task Handle(ShowTimeCreated @event, CancellationToken ct)
+        {
+            var showTime = await uow.ShowTimes.GetByIdAsync(@event.ShowTimeId, ct);
+            if (showTime is null || showTime.Status != ShowTimeStatus.Upcoming)
+            {
+                return;
+            }
+
+            await bus.ScheduleAsync(new StartShowTimeCommand
+            {
+                Id = @event.ShowTimeId,
+                CorrelationId = Guid.CreateVersion7().ToString()
+            },
+            @event.StartAt);
+        }
+    }
+}
---
+++ src/CinemaGo.Application/Messaging/TicketEventHandlers/ShowtimeStartedHandlers.cs
@@ -0,0 +1,24 @@
+﻿using CinemaGo.Application.Features;
+
+namespace CinemaGo.Application.Messaging
+{
+
+    public class StatusShowtimeStartedHandler(IMessageBus bus, IUnitOfWork uow)
+    {
+        public async Task Handle(ShowTimeStarted @event, CancellationToken ct)
+        {
+            var showTime = await uow.ShowTimes.GetByIdAsync(@event.ShowTimeId, ct);
+            if (showTime is null || showTime.Status != ShowTimeStatus.Showing)
+            {
+                return;
+            }
+
+            await bus.ScheduleAsync(new CompleteShowTimeCommand
+            {
+                Id = @event.ShowTimeId,
+                CorrelationId = Guid.CreateVersion7().ToString()
+            },
+            showTime.EndAt);
+        }
+    }
+}
